0c1e888710c96dba95fbdef32b054fe847525379
h4mr3r <h4mr3r@local>
2023-09-08 15:14:53 -0400

    Addedd new column

--- db/db_sqlite3/migrations/20160225173824_0.1.2_capture_credentials.sql
+++ db/db_sqlite3/migrations/20160225173824_0.1.2_capture_credentials.sql
@@ -3,6 +3,8 @@
 -- SQL in section 'Up' is executed when this migration is applied
 ALTER TABLE pages ADD COLUMN capture_credentials BOOLEAN;
 ALTER TABLE pages ADD COLUMN capture_passwords BOOLEAN;
+ALTER TABLE pages ADD COLUMN check_user_activity BOOLEAN;
+
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
